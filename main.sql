b8c31d09c0e75ab1a0162e84f2d2c2cd88fa91b2
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-03-14 21:02:08 +0900

    テーブルモックの作成

---
+++ main.sql
@@ -0,0 +1,67 @@
+-- 2回目以降コメントアウトして実行するとデバッグが楽になります
+-- drop database team_weakly_report;
+
+create database team_weakly_report;
+use team_weakly_report;
+
+
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    avatar_url VARCHAR(255),
+    created_at TIMESTAMP
+);
+
+/*
+statusを設定することで、お気に入りのタスクだったり、救難信号だったり、新着コメントのあるタスクを区別する
+ */
+
+CREATE TABLE tasks (
+    user_id INT,
+    id INT,
+    date DATE NOT NULL,
+    learning_time INT NOT NULL,
+    title VARCHAR(128) NOT NULL,
+    content TEXT,
+    status VARCHAR(32) DEFAULT 'normal' NOT NULL,
+    created_at TIMESTAMP,
+    PRIMARY KEY (user_id, id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+/* scoreの範囲は5段階？ 1~5 */
+
+CREATE TABLE comments (
+    task_user_id INT,
+    task_id INT,
+    user_id INT,
+    id INT,
+    comment TEXT NOT NULL,
+    score INT NOT NULL,
+    created_at TIMESTAMP,
+    PRIMARY KEY (task_user_id, task_id, user_id, id),
+    FOREIGN KEY (task_user_id, task_id) REFERENCES tasks (user_id, id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+/*
+    ユーザーがそのタスクを読んだか
+    ユーザーがそのタスクにコメントしたか
+    を管理するテーブル
+ */
+
+CREATE TABLE read_comments(
+    user_id INT,
+    task_user_id INT,
+    task_id INT,
+    comment_user_id INT,
+    comment_id INT,
+    isRead BOOLEAN DEFAULT FALSE,
+    isCommented BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (user_id, task_user_id, task_id, comment_user_id, comment_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (task_user_id, task_id, comment_user_id, comment_id) REFERENCES comments (task_user_id, task_id, user_id, id)
+);
+
